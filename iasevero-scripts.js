d82ccb91d24ddc20931926b127a08f354574b890
MARCOS JULIO SEVERO <severomarcos664@gmail.com>
2025-08-24 12:48:31 -0300

    Create iasevero-scripts.js

---
+++ iasevero-scripts.js
@@ -0,0 +1,10 @@
+// scroll suave para âncoras
+document.querySelectorAll('a[href^="#"]').forEach(a=>{
+  a.addEventListener('click', e=>{
+    const id = a.getAttribute('href');
+    if(id.length>1){
+      e.preventDefault();
+      document.querySelector(id)?.scrollIntoView({behavior:'smooth', block:'start'});
+    }
+  });
+});
